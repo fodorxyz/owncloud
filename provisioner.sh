9ab1cbeb665791ce38d02ff7285d23c055a7d10b
Ashley Hindle <hey@ashleyhindle.com>
2016-03-09 21:16:58 +0000

    set MySQL password

--- provisioner.sh
+++ provisioner.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
 
 wget -nv https://download.owncloud.org/download/repositories/stable/xUbuntu_14.04/Release.key -O Release.key
 apt-key add - < Release.key
 
 echo 'deb http://download.owncloud.org/download/repositories/stable/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud.list
 apt-get -y update
+
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysqlsecretpassword'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password mysqlsecretpassword'
+
 apt-get -y install owncloud php5-cli php5-curl php5-intl
