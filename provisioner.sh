c1c9ca45c95aa8ebdd4d3a756d5f47ba730fe99f
Ashley Hindle <hey@ashleyhindle.com>
2016-03-17 23:16:26 +0000

    Use new fodor features to provide the user with the username/password, setup MySQL for Owncloud instead of Sqlite

--- provisioner.sh
+++ provisioner.sh
@@ -10,4 +10,12 @@ apt-get -y update
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysqlsecretpassword'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password mysqlsecretpassword'
 
+mysql -uroot -pmysqlsecretpassword -e 'CREATE DATABASE IF NOT EXISTS owncloud;'
+mysql -uroot -pmysqlsecretpassword -e 'GRANT ALL ON owncloud.* to "owncloud"@"localhost" IDENTIFIED BY "owncloudsecret";'
+
+
 apt-get -y install owncloud php5-cli php5-curl php5-intl
+
+sudo -u www-data php /var/www/owncloud/occ maintenance:install --admin-user=fodor --admin-pass=fodor --database=mysql --database-name=owncloud --database-user owncloud --database-pass=owncloudsecret
+
+sed -i -e "s/0 => 'localhost',/0 => 'localhost',\n    1 => '$DOMAIN'/g" -e "s/http:\/\/localhost/http:\/\/$DOMAIN/g" /var/www/owncloud/config/config.php
