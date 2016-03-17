8d860289c0d88e401e950595706c867acef876c1
Ashley Hindle <hey@ashleyhindle.com>
2016-03-17 23:28:45 +0000

    set the mysql root password please

--- provisioner.sh
+++ provisioner.sh
@@ -12,10 +12,11 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password_again passwo
 
 mysql -uroot -pmysqlsecretpassword -e 'CREATE DATABASE IF NOT EXISTS owncloud;'
 mysql -uroot -pmysqlsecretpassword -e 'GRANT ALL ON owncloud.* to "owncloud"@"localhost" IDENTIFIED BY "owncloudsecret";'
+apt-get -y install mysql-server
 
 
 apt-get -y install owncloud php5-cli php5-curl php5-intl
 
-sudo -u www-data php /var/www/owncloud/occ maintenance:install --admin-user=fodor --admin-pass=fodor --database=mysql --database-name=owncloud --database-user owncloud --database-pass=owncloudsecret
+sudo -u www-data php /var/www/owncloud/occ maintenance:install --admin-user=fodor --admin-pass=fodor --database=mysql --database-name=owncloud --database-user=owncloud --database-pass=owncloudsecret
 
 sed -i -e "s/0 => 'localhost',/0 => 'localhost',\n    1 => '$DOMAIN'/g" -e "s/http:\/\/localhost/http:\/\/$DOMAIN/g" /var/www/owncloud/config/config.php
