1a3a9d5c5abed9504b8ccfcf1eeaf4a7ff06478a
Ashley Hindle <hey@ashleyhindle.com>
2016-07-07 21:47:04 +0100

    Adds random mysql passwords for setting up for added security using Fodor's new 'gen_password' feature

--- provisioner.sh
+++ provisioner.sh
@@ -7,16 +7,18 @@ apt-key add - < Release.key
 echo 'deb http://download.owncloud.org/download/repositories/stable/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud.list
 apt-get -y update
 
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysqlsecretpassword'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password mysqlsecretpassword'
+debconf-set-selections <<< "mysql-server mysql-server/root_password password ${RANDOM_PASSWORD}"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${RANDOM_PASSWORD}"
+
+export OWNCLOUD_DB_PASSWORD=$(gen_password)
 
 apt-get -y install mysql-server
-mysql -uroot -pmysqlsecretpassword -e 'CREATE DATABASE IF NOT EXISTS owncloud;'
-mysql -uroot -pmysqlsecretpassword -e 'GRANT ALL ON owncloud.* to "owncloud"@"localhost" IDENTIFIED BY "owncloudsecret";'
-
+mysql -u root -p '${RANDOM_PASSWORD}' -e 'CREATE DATABASE IF NOT EXISTS owncloud;'
+mysql -u root -p '${RANDOM_PASSWORD}' -e "GRANT ALL ON owncloud.* to 'owncloud'@'localhost' IDENTIFIED BY '${OWNCLOUD_DB_PASSWORD}';"
 
 apt-get -y install owncloud php5-cli php5-curl php5-intl
 
-sudo -u www-data php /var/www/owncloud/occ maintenance:install --admin-user=$ADMIN_USERNAME --admin-pass=$ADMIN_PASSWORD --database=mysql --database-name=owncloud --database-user=owncloud --database-pass=owncloudsecret
+
+sudo -u www-data php /var/www/owncloud/occ maintenance:install --admin-user=$ADMIN_USERNAME --admin-pass=$ADMIN_PASSWORD --database=mysql --database-name=owncloud --database-user=owncloud --database-pass=${OWNCLOUD_DB_PASSWORD}
 
 sed -i -e "s/0 => 'localhost',/0 => 'localhost',\n    1 => '$DOMAIN'/g" -e "s/http:\/\/localhost/http:\/\/$DOMAIN/g" /var/www/owncloud/config/config.php
